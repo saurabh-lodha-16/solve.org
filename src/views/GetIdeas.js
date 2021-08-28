ef4f3ed6ee2d7dd000cd61a337b96bd9eada3c42
Saurabh Lodha <saurabhlodha221b@gmail.com>
2021-08-28 20:11:04 +0530

    Minor fix in get ideas

--- src/views/GetIdeas.js
+++ src/views/GetIdeas.js
@@ -1,10 +1,9 @@
-import { useState } from 'react';
-import { ethers } from 'ethers';
+import { useState } from "react";
+import { ethers } from "ethers";
 
+import { requestAccount } from "../utils";
 
-import { requestAccount } from '../utils';
-
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button } from "@material-ui/core";
 
 // Importing this after TextField to get precedence
 import { useStyles } from "../styles";
@@ -16,61 +15,49 @@ import VotingContract from "../abis/VotingContract.json";
 
 const constants = require("../abis/contract-address.json");
 function GetIdeas() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const [ideas, setIdeas] = useState();
-    const [status, setStatus] = useState();
+  const [ideas, setIdeas] = useState();
+  const [status, setStatus] = useState();
 
-    async function getIdeas() {
-        setStatus("Loading...");
-        if (typeof window.ethereum != undefined) {
-            await requestAccount();
+  async function getIdeas() {
+    setStatus("Loading...");
+    if (typeof window.ethereum != undefined) {
+      await requestAccount();
 
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            const getIdeasContract = new ethers.Contract(
-                constants.VotingContract,
-                VotingContract.abi,
-                signer
-            );
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const getIdeasContract = new ethers.Contract(
+        constants.VotingContract,
+        VotingContract.abi,
+        signer
+      );
 
-            try {
-               
-                
-
-                
-                let ideasMappingTransaction = await getIdeasContract.ideas(1);
-                console.log(ideasMappingTransaction);
-                await ideasMappingTransaction.wait();
-
-               console.log(ideasMappingTransaction);
-               console.log(ideasMappingTransaction.events[2]);
-                
-            }
-            catch (err) {
-                console.log(err);
-                setStatus("idea fetching failed");
-            }
-        }
-
-
+      try {
+        let ideasMappingTransaction = await getIdeasContract.ideas(1);
+        console.log(ideasMappingTransaction);
+      } catch (err) {
+        console.log(err);
+        setStatus("idea fetching failed");
+      }
     }
+  }
 
-    return (
-        <div>
-            <center>
-                <Button
-                    className={classes.formElement}
-                    variant="contained"
-                    color="primary"
-                    onClick={getIdeas}>
-                    connect to metamask
-                </Button>
-                <p>{status}</p>
-            </center>
-        </div>
-    );
-
+  return (
+    <div>
+      <center>
+        <Button
+          className={classes.formElement}
+          variant="contained"
+          color="primary"
+          onClick={getIdeas}
+        >
+          connect to metamask
+        </Button>
+        <p>{status}</p>
+      </center>
+    </div>
+  );
 }
 
 export { GetIdeas };
