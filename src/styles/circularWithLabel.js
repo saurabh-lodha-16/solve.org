88633022c3393e1b47c5010831a698360ee7809a
Saurabh Lodha <saurabhlodha221b@gmail.com>
2021-08-29 07:13:58 +0530

    Fixed warnings

--- src/styles/circularWithLabel.js
+++ src/styles/circularWithLabel.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
@@ -20,18 +19,10 @@ export default function CircularProgressWithLabel(props) {
       >
         <Typography
           variant="caption"
-          component="div"
+          component="h5"
           color="secondary"
         >{`${Math.round(props.value)}%`}</Typography>
       </Box>
     </Box>
   );
 }
-
-CircularProgressWithLabel.propTypes = {
-  /**
-   * The value of the progress indicator for the determinate variant.
-   * Value between 0 and 100.
-   */
-  value: PropTypes.number.isRequired,
-};
